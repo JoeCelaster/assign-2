84105efee9005fb5b1c3c29ee038666fc41479b2
JoeCelaster <joecelaster2006@gmail.com>
2025-03-04 10:11:02 +0530

    assign-2

---
+++ new-assign-2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+const App = () => {
+  return (
+    <div className='joe'>
+      <h1>Joe Celaster</h1>
+      <h2>joecelstar@hotmail.com</h2>
+    </div>
+  );
+}
+
+export default App;
